16b0bc4d9e583b7fe37272f5d4dc7ea4d33449f2
aclozovoy <aclozovoy@gmail.com>
2022-08-04 23:33:21 -0700

    Scrape individual stats

--- datascraper.py
+++ datascraper.py
@@ -3,9 +3,20 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
+# Pull html from web
 url = 'https://www.baseball-reference.com/awards/hof_2022.shtml'
-
 r = requests.get(url)
 soup = BeautifulSoup(r.text, 'html.parser')
 
-print(soup)
+# Create empty dataframe
+df = pd.DataFrame()
+
+# Use only the body of the first table
+table = soup.body.table.tbody
+
+# Scrap stats from table
+for player in table.find_all('tr'):
+    for stat in player.find_all('td'):
+        print(stat.get_text())
+        print(stat.get('data-stat'))
+
