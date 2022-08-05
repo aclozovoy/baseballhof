4a2a668d6bb31e71618751e82c59e23ef4f0ec2b
aclozovoy <aclozovoy@gmail.com>
2022-08-05 16:15:55 -0700

    Update for tableau

--- datacleaner.py
+++ datacleaner.py
@@ -9,12 +9,15 @@ df['player_old'] = df['player']
 
 # Remove tags
 for index, row in df.iterrows():
-    name = row['player']
     #remove 'X-' from player names
+    name = row['player']
     if name[0:2] == 'X-':
         clean_name = name[2:]
         df.loc[index,['player']] = clean_name
+
+for index, row in df.iterrows(): 
     # Remove 'HOF' from player names
+    name = row['player']
     if name[-3:] == 'HOF':
         clean_name = name[0:-4]
         df.loc[index,['player']] = clean_name
